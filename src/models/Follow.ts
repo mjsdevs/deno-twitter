3d762274202ba3f6e67ef0423725863772ebf5b9
Deborah Espíndola <deo2@cin.ufpe.br>
2020-10-31 20:08:50 -0300

    feature(follow-model) change signal

--- src/models/Follow.ts
+++ src/models/Follow.ts
@@ -4,10 +4,10 @@ interface FollowSchema {
   _id: { $oid: string };
   followerId: {
     $oid: string,
-  },
+  };
   followedId: {
     $oid: string,
-  },
+  };
 };
 
 export default database.collection<FollowSchema>('Follows');
